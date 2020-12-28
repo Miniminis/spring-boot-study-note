2e8487f0883f299a5bba17e14c3150565a2085ec
miniminis <minhee4735@gmail.com>
2020-12-28 13:15:13 +0900

    [Admin Project] User Entity 생성

---
+++ study/src/main/java/com/example/study/model/User.java
@@ -0,0 +1,33 @@
+package com.example.study.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@Entity     //==table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
