596f2a82583b55712ff64e08f628e3abfcd8f0ab
miniminis <minhee4735@gmail.com>
2021-01-07 08:53:28 +0900

    [Admin Project] Item create() 생성 진행 중. - 통신은 snale, 소스는 camel case! - partnerId 조회 부분 오류가 있음. 수정 필요

---
+++ study/src/main/java/com/example/study/controller/api/ItemController.java
@@ -0,0 +1,46 @@
+package com.example.study.controller.api;
+
+import com.example.study.interfaces.CRUDInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.service.ItemApiService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/item")
+public class ItemController implements CRUDInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiService itemApiService;
+
+
+    @Override
+    @PostMapping("")
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> req) {
+        log.info("item create : {}", req);
+        return itemApiService.create(req);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> req) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header<ItemApiResponse> delete(Long id) {
+        return null;
+    }
+}
---
+++ study/src/main/java/com/example/study/service/ItemApiService.java
@@ -0,0 +1,73 @@
+package com.example.study.service;
+
+import com.example.study.interfaces.CRUDInterface;
+import com.example.study.model.entity.Item;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.repository.ItemRepository;
+import com.example.study.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ItemApiService implements CRUDInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> req) {
+        ItemApiRequest itemApiRequest = req.getData();
+
+        Item item = Item.builder()
+                .status(itemApiRequest.getStatus())
+                .name(itemApiRequest.getName())
+                .title(itemApiRequest.getTitle())
+                .content(itemApiRequest.getContent())
+                .price(itemApiRequest.getPrice())
+                .brandName(itemApiRequest.getBrandName())
+                .registeredAt(LocalDateTime.now())
+                .partner(partnerRepository.getOne(itemApiRequest.getPartnerId()))
+                .build();
+
+        Item newItem = itemRepository.save(item);
+        return response(newItem);
+    }
+
+    @Override
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> update(Header<ItemApiRequest> req) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> delete(Long id) {
+        return null;
+    }
+
+    private Header<ItemApiResponse> response(Item item) {
+        ItemApiResponse itemApiResponse = ItemApiResponse.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .title(item.getTitle())
+                .content(item.getContent())
+                .price(item.getPrice())
+                .brandName(item.getBrandName())
+                .registeredAt(item.getRegisteredAt())
+                .unregisteredAt(item.getUnregisteredAt())
+                .partnerId(item.getPartner().getId())
+                .build();
+        return Header.OK(itemApiResponse);
+    }
+}
