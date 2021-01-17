4fd2df485abb5870bce29ae5b1064af7e9e5dc13
miniminis <minhee4735@gmail.com>
2021-01-18 06:20:48 +0900

    [MyContact] lombok 각 어노테이션의 의미 및 사용

--- mycontact/src/main/java/com/example/facebook/mycontact/domain/Person.java
+++ mycontact/src/main/java/com/example/facebook/mycontact/domain/Person.java
@@ -1,23 +1,60 @@
 package com.example.facebook.mycontact.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.sun.istack.NotNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@RequiredArgsConstructor        //+@NonNull : 꼭 필요한 field 만 생성자로 만들 수 있음!
+@Data                           //equalsAndHash 도 같이 설정되어잇음 : 내부에 들어가서 이미 세팅되어있는 요소들 확인 필요!
+@ToString(exclude = {"phoneNumber", "job"})     //ToString 요소에서 제외해줄 필드 설정
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    @NonNull
     private String name;
-    private int age;
+
+    @NonNull
+    private Integer age;
+
+    private String bloodType;
+
+    private String address;
+
+    private LocalDate birthDay;
+
+    private String job;
+
+    private String phoneNumber;
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Person person = (Person) o;
+//        return Objects.equals(id, person.id) &&
+//                Objects.equals(name, person.name) &&
+//                Objects.equals(age, person.age) &&
+//                Objects.equals(bloodType, person.bloodType) &&
+//                Objects.equals(address, person.address) &&
+//                Objects.equals(birthDay, person.birthDay) &&
+//                Objects.equals(job, person.job) &&
+//                Objects.equals(phoneNumber, person.phoneNumber);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, name, age, bloodType, address, birthDay, job, phoneNumber);
+//    }
 }
--- mycontact/src/test/java/com/example/facebook/mycontact/interfaces/PersonRepositoryTest.java
+++ mycontact/src/test/java/com/example/facebook/mycontact/interfaces/PersonRepositoryTest.java
@@ -20,15 +20,27 @@ class PersonRepositoryTest{
     @Test
     @Transactional
     public void crud() {
-        Person person = new Person();
-        person.setName("james");
-        person.setAge(10);
+//        Person person = new Person();
+        Person person = new Person("james", 10);
+//        person.setName("james");
+//        person.setAge(10);
         personRepository.save(person);
 
         System.out.println(personRepository.findAll());
 
-        List<Person> personList =  personRepository.findAll();
-        assertEquals(personList.size(), 1);
-        assertEquals(personList.get(0).getName(), "james");
+//        List<Person> personList =  personRepository.findAll();
+//        assertEquals(personList.size(), 1);
+//        assertEquals(personList.get(0).getName(), "james");
+    }
+
+    @Test
+    @Transactional
+    public void equalsAndHashCodeTest() {
+        Person person1 = new Person("james", 10);
+        Person person2 = new Person("james", 10);
+
+        assertEquals(person1, person2);
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
     }
 }
