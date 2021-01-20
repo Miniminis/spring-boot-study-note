5e2e9b7810908006fd66747e99d495ebe54239f6
miniminis <minhee4735@gmail.com>
2021-01-21 08:05:10 +0900

    [MyContact] QueryMethod 활용 1 : findByName, findByBlockIsNull, 8월 생일자 알림 기능 구현 진행중

--- mycontact/src/main/java/com/example/facebook/mycontact/interfaces/PersonRepository.java
+++ mycontact/src/main/java/com/example/facebook/mycontact/interfaces/PersonRepository.java
@@ -3,6 +3,14 @@ package com.example.facebook.mycontact.interfaces;
 import com.example.facebook.mycontact.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    List<Person> findByName(String name);
+
+    List<Person> findByBlockIsNull();
+
+    List<Person> findByBirthDayBetween(LocalDate startDate, LocalDate endDate);
 }
--- mycontact/src/test/java/com/example/facebook/mycontact/interfaces/PersonRepositoryTest.java
+++ mycontact/src/test/java/com/example/facebook/mycontact/interfaces/PersonRepositoryTest.java
@@ -1,19 +1,87 @@
 package com.example.facebook.mycontact.interfaces;
 
+import com.example.facebook.mycontact.domain.Block;
 import com.example.facebook.mycontact.domain.Person;
+import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class PersonRepositoryTest{
+class PersonRepositoryTest {
 
     @Autowired
     private PersonRepository personRepository;
+
+
+    void givenPerson(String name, int age, String bloodType) {
+        givenPerson(name, age, bloodType, null);
+    }
+
+
+    void givenPerson(String name, int age, String bloodType, Block block) {
+        givenPerson(name, age, bloodType, block, null);
+    }
+
+    void givenPerson(String name, int age, String bloodType, Block block, LocalDate birthDay) {
+        Person person = new Person(name, age);
+        person.setBloodType(bloodType);
+        person.setBlock(block);
+        person.setBirthDay(birthDay);
+
+        personRepository.save(person);
+    }
+
+
+    @Test
+    @Transactional
+    void findByBirthDateBetweenTest() {
+        givenPerson("martin", 10, "A", new Block("james"),
+                LocalDate.of(1994, 8, 11));
+        givenPerson("james", 12, "B", null,
+                LocalDate.of(1990, 12, 19));
+        givenPerson("selly", 38, "AB", new Block("james"),
+                LocalDate.of(1988, 8, 22));
+        givenPerson("martin", 120, "A", null,
+                LocalDate.of(1993, 5, 24));
+
+        List<Person> personList = personRepository.findByBirthDayBetween(LocalDate.of(1988, 8, 1),
+                LocalDate.of(1994, 8, 31));
+
+        personList.forEach(System.out::println);
+    }
+
+
+    @Test
+    void findByBlockIsNullTest() {
+        givenPerson("martin", 10, "A", new Block("james"));
+        givenPerson("james", 12, "B");
+        givenPerson("selly", 38, "AB", new Block("james"));
+        givenPerson("martin", 120, "A");
+
+        List<Person> personList = personRepository.findByBlockIsNull();
+        personList.forEach(System.out::println);
+    }
+
+
+    @Test
+    void findByNameTest() {
+        givenPerson("martin", 10, "A");
+        givenPerson("james", 12, "B");
+        givenPerson("selly", 38, "AB");
+        givenPerson("martin", 120, "A");
+
+        List<Person> personList = personRepository.findByName("martin");
+        personList.forEach(System.out::println);
+    }
+
 
     @Test
     @Transactional
