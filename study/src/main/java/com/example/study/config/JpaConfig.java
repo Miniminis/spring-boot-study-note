e0fb991f71045708e0f7067c7eacd3c953ab9a02
miniminis <minhee4735@gmail.com>
2021-02-10 14:02:49 +0900

    [study] 쓰인 sqls, erd_model 프로젝트에 포함

--- study/src/main/java/com/example/study/config/JpaConfig.java
+++ study/src/main/java/com/example/study/config/JpaConfig.java
@@ -1,6 +1,5 @@
 package com.example.study.config;
 
-import lombok.EqualsAndHashCode;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
