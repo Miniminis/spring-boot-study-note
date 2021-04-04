9991975dcf979868512e9b1f0569e1ad5c8d4ac3
miniminis <minhee4735@gmail.com>
2021-04-05 04:19:16 +0900

    move zaliving-test to other repo

--- zaliving/zaliving-common/src/main/java/com/example/zaliving/repositories/HouseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.zaliving.repositories;
-
-import com.example.zaliving.domains.House;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HouseRepository extends JpaRepository<House, Long> {
-}
--- zaliving/zaliving-common/src/main/java/com/example/zaliving/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.zaliving.repositories;
-
-import com.example.zaliving.domains.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
