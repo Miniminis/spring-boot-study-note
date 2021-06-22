3565687a91afafe8e27f5102fbfdf8c987958727
flashMhSon <flash@holostanding.space>
2021-06-22 18:52:55 +0900

    Class2.ExecutionContext in Job and Step - Job 과 Step은 각각 execution 과 executionContext 를 가진다. - Job 의 executionContext 는 Job 이하의 모든 step 에게서 공통으로 쓰일 수 있지만, - step 의 executionContext 는 step 단위로만 유효하여 다른 step 에서 공유될 수 없다. --> 마치 android 의 context 와 비슷한 개념처럼 느껴졌다.

---
+++ spring-batch/src/main/java/com/example/batch/sample/part2/SharedConfig.java
@@ -0,0 +1,82 @@
+package com.example.batch.sample.part2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class SharedConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    public SharedConfig(JobBuilderFactory jobBuilderFactory,
+                        StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job sharedJob() {
+        return jobBuilderFactory.get("sharedJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.sharedStep())
+                .next(this.sharedStep2())
+                .build();
+    }
+
+    @Bean
+    public Step sharedStep() {
+        return stepBuilderFactory.get("sharedStep")
+                .tasklet((contribution, chunkContext) -> {
+                    StepExecution stepExecution = contribution.getStepExecution();
+                    ExecutionContext stepExecutionContext = stepExecution.getExecutionContext();
+                    stepExecutionContext.put("stepKey", "step execution context");
+
+                    JobExecution jobExecution = stepExecution.getJobExecution();
+                    ExecutionContext jobExecutionExecutionContext = jobExecution.getExecutionContext();
+                    jobExecutionExecutionContext.put("jobKey", "job execution context");
+
+                    JobInstance jobInstance = jobExecution.getJobInstance();
+                    JobParameters jobParameters = jobExecution.getJobParameters();
+
+                    log.info("jobName: {}, stepName: {}, parameter:{}",
+                            jobInstance.getJobName(),
+                            stepExecution.getStepName(),
+                            jobParameters.getLong("run.id")
+                            );
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step sharedStep2() {
+        return stepBuilderFactory.get("sharedStep02")
+                .tasklet((contribution, chunkContext) -> {
+                    StepExecution stepExecution = contribution.getStepExecution();
+                    ExecutionContext stepExecutionExecutionContext = stepExecution.getExecutionContext();
+
+                    JobExecution jobExecution = stepExecution.getJobExecution();
+                    ExecutionContext jobExecutionExecutionContext = jobExecution.getExecutionContext();
+
+                    log.info("jobKey: {}, stepKey: {}",
+                            jobExecutionExecutionContext.getString("jobKey", "noJobKey"),
+                            stepExecutionExecutionContext.getString("stepKey", "noStepKey")
+                            );
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+
+    }
+}
