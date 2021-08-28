bf481d67517f6e7be506420e1191303bd209be79
minhee.son <minhee4735@gmail.com>
2021-08-28 09:36:29 +0900

    feat @Component 로 Encoder 관리하도록 변경

--- spring-ioc/src/main/java/com/example/ioc/IocApplication.java
+++ spring-ioc/src/main/java/com/example/ioc/IocApplication.java
@@ -1,8 +1,6 @@
 package com.example.ioc;
 
-import com.example.ioc.encoders.Base64Encoder;
 import com.example.ioc.encoders.Encoder;
-import com.example.ioc.encoders.UrlEncoder;
 import com.example.ioc.providers.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
