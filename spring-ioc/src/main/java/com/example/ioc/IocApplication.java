e000d90e84f429e99b3c193ce7f0289a4ebaa61b
minhee.son <minhee4735@gmail.com>
2021-08-28 10:14:58 +0900

    feat @Configuration 으로 여러개의 빈 등록하여 관리할 수 있도록 구조 변경

--- spring-ioc/src/main/java/com/example/ioc/IocApplication.java
+++ spring-ioc/src/main/java/com/example/ioc/IocApplication.java
@@ -1,10 +1,15 @@
 package com.example.ioc;
 
+import com.example.ioc.encoders.Base64Encoder;
 import com.example.ioc.encoders.Encoder;
+import com.example.ioc.encoders.IEncoder;
+import com.example.ioc.encoders.UrlEncoder;
 import com.example.ioc.providers.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class IocApplication {
@@ -15,9 +20,24 @@ public class IocApplication {
 
         String message = "https://newsstand.naver.com/";
 
-        Encoder encoder = context.getBean(Encoder.class);
+        Encoder encoder = context.getBean("urlEncoderBean", Encoder.class);
         String encodedMessage = encoder.encode(message);
         System.out.println(encodedMessage);     //aHR0cHM6Ly9uZXdzc3RhbmQubmF2ZXIuY29tLw==
     }
+}
+
+@Configuration
+class AppConfig {
+
+    @Bean("base64EncoderBean")
+    public Encoder encode(Base64Encoder base64Encoder) {
+        return new Encoder(base64Encoder);
+    }
+
+    @Bean("urlEncoderBean")
+    public Encoder encode(UrlEncoder urlEncoder) {
+        return new Encoder(urlEncoder);
+    }
+
 
 }
