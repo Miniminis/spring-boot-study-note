3d9cfc42fabc8aa860278c6b98ba5f012a4251f0
minhee.son <minhee4735@gmail.com>
2021-09-18 15:35:07 +0900

    feat arirang-first project create

---
+++ arirang-first/src/main/java/com/example/arirangfirst/ArirangFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.arirangfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArirangFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArirangFirstApplication.class, args);
+    }
+
+}
---
+++ arirang-first/src/test/java/com/example/arirangfirst/ArirangFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arirangfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArirangFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
